d4c9c9e245ecbf1a3700110dd11efb724e06a658
Harish Gudla <harish@kemi.uu.se>
2024-01-18 12:04:21 +0100

    update

---
+++ tutorial/PEO-LiTFSI_old/PEO.acpype/rungmx.sh
@@ -0,0 +1,6 @@
+
+echo 0 | gmx editconf -f PEO_GMX.gro -bt octahedron -d 1 -c -princ
+gmx grompp -f em.mdp -c out.gro -p PEO_GMX.top -o em.tpr -v
+gmx mdrun -ntmpi 1 -v -deffnm em
+gmx grompp -f md.mdp -c em.gro -p PEO_GMX.top -o md.tpr -r em.gro
+gmx mdrun -ntmpi 1 -v -deffnm md
